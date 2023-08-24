8f2d36df26cd8f6f7642091e0686ebef215cadf0
Prakhar Rai <prakharrai@ip-172-18-12-103.ap-south-1.compute.internal>
2023-08-24 13:17:28 +0530

    Project init

---
+++ src/pages/contact/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Contact = () => {
+  return <div id="content">
+    <header>
+      <p class="name-header"><a href="/">← Prakhar Rai</a></p>
+    </header>
+
+    <h1 class="dateless">Contact</h1>
+
+    <p>I'm happy to hear from you:</p>
+
+    <ul>
+      <li><a href="mailto:rai.prakhar1609@gmail.com">rai.prakhar1609@gmail.com</a></li>
+      <li><a href="https://twitter.com/prakharrai1609">@twitter</a></li>
+      <li><a href="https://linkedin.com/in/prakharrai1609">@linkedin</a></li>
+    </ul>
+
+    <p>I reply to every thoughtful note!</p>
+
+  </div>
+}
+
+export default Contact
