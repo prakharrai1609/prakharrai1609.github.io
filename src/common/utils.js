db03b4588de18117c5ac95b02319e6b7ad20521c
Prakhar <rai.prakhar1609@gmail.com>
2023-09-09 09:26:40 +0530

    cleanup

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,18 +2,4 @@ export class Utils {
   static async sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
-
-  static async typeWriter(texts, setTextElement) {
-    for (const text of texts) {
-      for (const char of text) {
-        await this.sleep(30);
-        setTextElement(line => line + char);
-      }
-
-      if (texts.indexOf(text) < texts.length - 1) {
-        await this.sleep(1000);
-        setTextElement(line => line + "<br /><br />");
-      }
-    }
-  }
 };
