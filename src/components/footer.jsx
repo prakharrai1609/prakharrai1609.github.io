c0ddd90253d3cb5861eead6005d830b88cf87ff5
Prakhar <rai.prakhar1609@gmail.com>
2023-09-10 21:05:27 +0530

    added footer links

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,46 +1,16 @@
 import React from "react";
+import SocialsButtons from "./socialsButtons";
 
 const Footer = () => {
   return (
-    <footer
-      style={{ boxShadow: "rgba(0, 0, 0, 0.35) 0px 0px 2px" }}
-      id="footer"
-    >
+    <footer id="footer">
       <div
         style={{ display: "flex", alignItems: "center" }}
         className="credits"
       >
         &#9400; Prakhar Rai, 2023
       </div>
-      <div
-        style={{ display: "flex", flexDirection: "row" }}
-        class="social-links"
-      >
-        <span className="footerSocials">
-          <a href="https://www.linkedin.com/in/prakharrai1609" target="_blank">
-            <image
-              src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-line-icon.png"
-              alt="LinkedIn"
-            />
-          </a>
-        </span>
-        <span className="footerSocials">
-          <a href="https://github.com/prakharrai1609/" target="_blank">
-            <image
-              src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png"
-              alt="GitHub"
-            />
-          </a>
-        </span>
-        <span className="footerSocials">
-          <a href="https://twitter.com/prakharrai1609/" target="_blank">
-            <image
-              src="https://cdn-icons-png.flaticon.com/512/81/81609.png"
-              alt="GitHub"
-            />
-          </a>
-        </span>
-      </div>
+      <SocialsButtons />
     </footer>
   );
 };
---
+++ src/components/socialsButtons.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const SocialsButtons = () => {
+  return (
+    <div style={{ display: "flex", flexDirection: "row" }} class="social-links">
+      <span className="footerSocials">
+        <button
+          style={{
+            border: "1px solid #0077b5",
+            borderRadius: "25px",
+            padding: "4px 10px 4px",
+          }}
+          className="btn btn-primary"
+        >
+          <a
+            href="https://www.linkedin.com/in/prakharrai1609"
+            target="_blank"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            LinkedIn
+          </a>
+        </button>
+      </span>
+      <span className="footerSocials">
+        <button
+          style={{
+            border: "1px solid black",
+            borderRadius: "25px",
+            padding: "4px 10px 4px",
+          }}
+          className="btn btn-dark"
+        >
+          <a
+            href="https://twitter.com/prakharrai1609/"
+            target="_blank"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Twitter
+          </a>
+        </button>
+      </span>
+      <span className="footerSocials">
+        <button
+          style={{
+            border: "1px solid #f5f5f5",
+            borderRadius: "25px",
+            padding: "4px 10px 4px",
+            backgroundColor: "#f5f5f5",
+          }}
+          className="btn"
+        >
+          <a
+            href="https://github.com/prakharrai1609/"
+            target="_blank"
+            style={{ textDecoration: "none" }}
+          >
+            Github
+          </a>
+        </button>
+      </span>
+    </div>
+  );
+};
+
+export default SocialsButtons;
